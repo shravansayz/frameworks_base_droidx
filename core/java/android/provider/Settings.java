2c84e5ff33e52bd7e75fc880830aff855e0d2a83
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-16 06:00:12 +0000

    SystemUI: Make round QS toggleable [1/3]
    Change-Id: I86e3bc990b78e763bfc9fb4a7ed8ffeeff95cc3f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12715,6 +12715,12 @@ public final class Settings {
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
         /**
+         * Quicksettings UI Style
+         * @hide
+         */
+        public static final String QS_STYLE_ROUND = "qs_style_round";
+
+        /*
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
